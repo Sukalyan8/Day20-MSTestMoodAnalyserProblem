9a11427561dffdf9331d1693294ef723e76e80ba
Sukalyan <sukalyandash1920@gmail.com>
2022-01-27 18:09:55 +0530

    mood analyse by exception handling

---
+++ MoodAnalyzeExceptionHandle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzeExceptionHandle
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            try
+            {
+
+                if (message.ToLower().Contains("happy"))
+                {
+                    return "happy";
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                return "happy";
+
+            }
+        }
+    }
+}
